3be3d8c6f9a7c2b84be614db5ece0c4c44c99514
PCoelho07 <pedro_coelho_pv@hotmail.com>
2016-12-31 00:45:17 -0300

    Function evaluate added

--- simple_calc.py
+++ simple_calc.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from pyparsing import *
+import re
+
+exprStack = []
+
+def pushElement(s, loc, tok):
+	exprStack.append(tok[0])
+	print(exprStack)
 
 def BNF():
 	global bnf
@@ -12,12 +19,12 @@ def BNF():
 	menos = Literal('-')
 	igual = Literal('=')
 	operator = (mais | menos)
-	atomo = (coef | numero)
+	atomo = (coef | numero).setParseAction(pushElement)
 
 	expr = Forward()
 	expr_final = Forward()
 
-	expr <<	atomo + ZeroOrMore(operator + atomo)
+	expr <<	atomo + ZeroOrMore((operator + atomo).setParseAction(pushElement))
 
 	expr_final << expr + igual + numero
 
@@ -28,19 +35,43 @@ def BNF():
 def handle_terms(parsed_expr):
  	coef_list = []
  	numbers = []
- 	for st in parsed_expr:
- 		if st.find("x") > -1:
- 			coef_list.append(st)
- 		elif isinstance(int(st), int) :
- 			numbers.append(st)
- 	
- 	return numbers, coef_list
+ 	equal = []
+
+ 	equal.append(parsed_expr[len(parsed_expr)-1])
+
+ 	for st in range(0, len(parsed_expr)-2):
+ 		if parsed_expr[st].find("x") > -1:
+ 			coef_list.append(parsed_expr[st])
+ 		elif parsed_expr[st].isdigit():
+ 			numbers.append(parsed_expr[st])
 
 
 
+ 	return numbers, coef_list, equal, parsed_expr
 
 
-op = {
+def evaluate(s): 
+	op = s.pop()
+	if op in "+-":
+		op2 = evaluate(s)
+		op1 = evaluate(s)
+		return operations[op](op1, op2)
+
+	elif re.search('^[0-9]*[a-zA-Z]$', op):
+		st = []
+		for x in list(op):
+			if x.isdigit():
+				st.append(x)
+
+		st = ''.join(st)
+		return int(st)
+
+	elif op.isdigit():
+		return int(op)
+		
+
+
+operations = {
 	'+': lambda x, y: x + y,
 	'-': lambda x, y: x - y,
 }
